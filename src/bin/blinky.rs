64a8fa1bbd9eb616ec77a135484084fce0ae3657
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2024-05-01 10:10:54 +0800

    All LEDs blink

--- src/bin/blinky.rs
+++ src/bin/blinky.rs
@@ -23,16 +23,26 @@ async fn main(_spawner: Spawner) {
     let peripherals = embassy_stm32::init(clock_config());
 
     // Create a new output pin - PA9 is the green led on the Discovery board
-    let mut led = Output::new(peripherals.PA9, Level::High, Speed::VeryHigh);
+    let mut green_led = Output::new(peripherals.PA9, Level::High, Speed::VeryHigh);
+    let mut red_led = Output::new(peripherals.PD5, Level::High, Speed::VeryHigh);
+    let mut green_led2 = Output::new(peripherals.PD12, Level::High, Speed::VeryHigh);
+    let mut orange_led = Output::new(peripherals.PD13, Level::High, Speed::VeryHigh);
+    let mut red_led2 = Output::new(peripherals.PD14, Level::High, Speed::VeryHigh);
+    let mut blue_led = Output::new(peripherals.PD15, Level::High, Speed::VeryHigh);
 
     // Create a new Ticker for the delay
-    let mut ticker = Ticker::every(Duration::from_millis(500));
+    let mut ticker = Ticker::every(Duration::from_millis(100));
 
     loop {
         // Wait for the ticker to expire
         ticker.next().await;
 
-        // Toggle the led
-        led.toggle();
+        // Toggle the leds
+        green_led.toggle();
+        red_led.toggle();
+        green_led2.toggle();
+        orange_led.toggle();
+        red_led2.toggle();
+        blue_led.toggle();
     }
 }
