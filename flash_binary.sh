e051fc369e19be823e1025796ea141a618aabe40
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2024-05-01 10:02:40 +0800

    Blinking the green LED works

--- flash_binary.sh
+++ flash_binary.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+# Get the first argument and if it does not exist set it to the default.
+# This is the name of the binary to compile.
+BINARY_NAME=${1:-blinky}
+
 # Echo the command to the console
 set -x
 
-BINFILE="firmware.bin"
 #echo $BINFILE
-st-flash --connect-under-reset write ${BINFILE} 0x8000000
+#st-flash --connect-under-reset write firmware/${BINARY_NAME}.bin 0x8000000
+cp firmware/${BINARY_NAME}.bin /Volumes/DIS_F407VG
--- src/bin/blinky.rs
+++ src/bin/blinky.rs
@@ -2,10 +2,17 @@
 #![no_main]
 
 use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::{Duration, Ticker};
 use panic_probe as _;
 
 fn clock_config() -> embassy_stm32::Config {
-    let config = embassy_stm32::Config::default();
+    use embassy_stm32::rcc::*;
+
+    let mut config = embassy_stm32::Config::default();
+
+    // Configure to use the high speed internal oscillator (HSI).
+    config.rcc.hsi = true;
 
     config
 }
@@ -13,7 +20,19 @@ fn clock_config() -> embassy_stm32::Config {
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     // Initialize embassy
-    embassy_stm32::init(clock_config());
+    let peripherals = embassy_stm32::init(clock_config());
 
-    loop {}
+    // Create a new output pin - PA9 is the green led on the Discovery board
+    let mut led = Output::new(peripherals.PA9, Level::High, Speed::VeryHigh);
+
+    // Create a new Ticker for the delay
+    let mut ticker = Ticker::every(Duration::from_millis(500));
+
+    loop {
+        // Wait for the ticker to expire
+        ticker.next().await;
+
+        // Toggle the led
+        led.toggle();
+    }
 }
