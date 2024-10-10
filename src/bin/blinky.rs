987db9b48ac6bcda205b6bcbc921c844dc687752
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2024-10-10 23:09:39 +0800

    saving

--- src/bin/blinky.rs
+++ src/bin/blinky.rs
@@ -3,9 +3,20 @@
 
 use cortex_m::asm;
 use panic_probe as _;
+use stm32f4xx_hal::{gpio::GpioExt, pac};
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
+    // let mut green_led = Output::new(peripherals.PA9, Level::High, Speed::VeryHigh);
+    // let mut red_led = Output::new(peripherals.PD5, Level::High, Speed::VeryHigh);
+    // let mut green_led2 = Output::new(peripherals.PD12, Level::High, Speed::VeryHigh);
+    // let mut orange_led = Output::new(peripherals.PD13, Level::High, Speed::VeryHigh);
+    // let mut red_led2 = Output::new(peripherals.PD14, Level::High, Speed::VeryHigh);
+    // let mut blue_led = Output::new(peripherals.PD15, Level::High, Speed::VeryHigh);
+
+    let peripherals = pac::Peripherals::take().unwrap();
+    let gpiod = &peripherals.GPIOD.split();
+
     loop {
         for _ in 0..1_000_000 {
             asm::nop();
